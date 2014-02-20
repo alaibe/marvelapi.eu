082f212a5d07ef713f62f29ae405dbebc7add451
Anthony Laibe <anthony@laibe.cc>
2014-02-20 22:38:53 +0000

    Add header to show view

--- app/scripts/controllers/show.js
+++ app/scripts/controllers/show.js
@@ -1,6 +1,8 @@
 'use strict';
 
 angular.module('marvelApi')
-.controller('ShowCtrl', function ($scope, $routeParams, MarvelApiService) {
+.controller('ShowCtrl', function ($scope, $routeParams, MarvelApiService, Model) {
+  $scope.models = Model.all;
+  $scope.title = $routeParams.type;
   MarvelApiService.find($routeParams.type, $routeParams.id, $scope);
 });
