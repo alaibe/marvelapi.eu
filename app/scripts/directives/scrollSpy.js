74a2ce0d36f77bc58182b9c2ce746a49bc3effcc
Anthony Laibe <anthony@laibe.cc>
2014-02-21 22:56:04 +0000

    Scroll spy

--- app/scripts/directives/scrollSpy.js
+++ app/scripts/directives/scrollSpy.js
@@ -3,26 +3,63 @@ angular.module('marvelApi').directive('preventDefault', function() {
     jQuery(element).click(function(event) {
       event.preventDefault();
     });
-  }
+  };
 });
 
-angular.module('marvelApi').directive("scrollTo", ["$window", function($window){
+angular.module('marvelApi').directive("spy", function(){
+  return {
+    restrict: "A",
+    require: "^scrollSpy",
+    link: function($scope, $elem, $attrs, affix){
+      affix.addSpy({
+        id: $attrs.spy,
+        in: function(){
+          $elem.addClass('active');
+        },
+        out: function(){
+          $elem.removeClass('active');
+        }
+      });
+    }
+  };
+});
+
+angular.module('marvelApi').directive("scrollSpy", function($window){
   return {
     restrict : "AC",
-    compile : function(){
+    controller: function($scope) {
+      $scope.spies = [];
 
-      function scrollInto(elementId) {
-        if(!elementId) $window.scrollTo(0, 0);
-        var el = document.getElementById(elementId);
-        if(el) el.scrollIntoView();
-      }
-      return function(scope, element, attr) {
-        element.bind("click", function(event){
-          $(event.toElement).parent().parent().children().removeClass("active");
-          $(event.toElement).parent().addClass("active");
-          scrollInto(attr.scrollTo);
+      this.addSpy = function(spyObj) {
+        return $scope.spies.push(spyObj);
+      };
+    },
+
+    link: function($scope, $elem, $attrs) {
+      $($window).scroll(function() {
+        var highlightSpy = null;
+        angular.forEach($scope.spies, function(spy, key){
+          spy.out();
+          var elemWithId = $elem.find('#'+spy.id);
+
+          if ((pos = elemWithId.offset().top) - $window.scrollY <= 0){
+            spy.pos = pos
+            if (highlightSpy == null) {
+              highlightSpy = spy;
+            }
+            if (highlightSpy.pos < spy.pos) {
+              highlightSpy = spy;
+            }
+          }
         });
-      }
+
+        if (highlightSpy != null) {
+          highlightSpy.in();
+        } else {
+          $scope.spies[0].in();
+        }
+
+      });
     }
-  }
-}]);
+  };
+});
